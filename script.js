bcda5d037b16648da73ccc9b1311be59f157e4e2
Abdullah Zafar <136666960+2Toeasty@users.noreply.github.com>
2023-08-24 15:38:46 -0400

    finalize pseudocode

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-/*
-Global Variables:
-computerScore
-*/
+
 
 /*
 functions needed:
 Player Choice
 Computer Choice
-Win determiner
+Win Decider
 Win Counter
 */
 
@@ -28,20 +25,32 @@ if the number is 3, return "SCISSORS"
 */
 
 /*
+create integer playerScore == 0
+create integer computerScore == 0
+
 create a loop that runs through the game until either computerScore == 5, or playerScore == 5
 
 string output = "the computer chose " [computerChoice]
+String playerChoice = playerChoice()
+String computerChoice = computerChoice()
+int winner = WinDecider(playerChoice, computerChoice)
 
-if WinDecider == 0, output += "it's a tie", move to the next game.
-if WinDecider == 1, output += "you win", playerScore++
-if WinDecider == 2, output += "you lose", computerScore++
+if winner == 0, output += "it's a tie", move to the next game.
+if winner == 1, output += "you win", playerScore++
+if winner == 2, output += "you lose", computerScore++
 output "the score is: /nComputer: computerScore /nPlayer: playerScore"
+
+[once loop has exited]
+
+if playerScore == 5, output "Congratulations, you won the game!"
+    else
+        "Game over, you lost :("
 
 */
 
 /*
-WinDecider
-if playerChoice equals computerChoice, output "the computer chose [computerChoice], it's a tie!" return 0
+WinDecider (playerChoice, computerChoice)
+if playerChoice equals computerChoice, return 0
 if playerChoice is ROCK,
     then if computerChoice is SCISSORS
         return 1
