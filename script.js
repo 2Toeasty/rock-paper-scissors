e133deb455ddd3b675eab2fea8484784d3a7fa4c
Abdullah Zafar <136666960+2Toeasty@users.noreply.github.com>
2024-05-04 16:14:50 -0400

    create functional buttons; bug: one button press remains throughout all rounds of the game

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 
-function getPlayerChoice() { //TODO: Change prompts to button options
-    let playerChoice = prompt("Enter Rock, Paper, or Scissors");
+function getPlayerChoice(choseRock, chosePaper, choseScissors) { //TODO: Change prompts to button options
+
+    if (choseRock == true) {
+        return "ROCK";
+    } else if (chosePaper == true) {
+        return "PAPER";
+    } else if (choseScissors == true) {
+        return "SCISSORS";
+    } else {
+        alert("No Individual Button Press Detected");
+    }
+
+    /*let playerChoice = prompt("Enter Rock, Paper, or Scissors");
     playerChoice = playerChoice.toUpperCase();
     while (playerChoice != "ROCK" && playerChoice != "PAPER" && playerChoice != "SCISSORS") {
         playerChoice = prompt("Please enter a valid move");
         playerChoice = playerChoice.toUpperCase();
-    }
+    }*/
     return playerChoice;
 }
 
@@ -27,81 +38,95 @@ let playerScore = 0;
 let computerScore = 0;
 const result = document.querySelector("p");
 
-function playRound(playerScore, computerScore) {
+function playRound(playerScore, computerScore, rockPress, paperPress, scissorsPress) {
     if (result) {
         result.textContent = ""; //resets the result div
     }
-        while (playerScore != 5 && computerScore != 5) {
+    while (playerScore != 5 && computerScore != 5) {
 
-            let playerChoice = getPlayerChoice();
-            let computerChoice = getComputerChoice();
 
-            let output = "You chose " + playerChoice + "\n The computer chose " + computerChoice + ", ";
-            let winner = WinDecider(playerChoice, computerChoice);
+        let playerChoice = getPlayerChoice(rockPress, paperPress, scissorsPress);
+        let computerChoice = getComputerChoice();
 
-            switch (winner) {
-                case 0:
-                    output += `it's a tie, the score is still: \n Computer: ${computerScore} \n You: ${playerScore}`;
-                    result.textContent = output;
-                    break;
-                case 1:
-                    playerScore++;
-                    output += `you win! The score is now: \n Computer: ${computerScore} \n You: ${playerScore}`;
-                    result.textContent = output;
-                    break;
-                case 2:
-                    computerScore++;
-                    output += `you lose. The score is now: \n Computer: ${computerScore} \n You: ${playerScore}`;
-                    result.textContent = output; //TODO: Merge the three identical lines of code into one
-            }
+        let output = "You chose " + playerChoice + "\n The computer chose " + computerChoice + ", ";
+        let winner = WinDecider(playerChoice, computerChoice);
+
+        switch (winner) {
+            case 0:
+                output += `it's a tie, the score is still: \n Computer: ${computerScore} \n You: ${playerScore}`;
+                result.textContent = output;
+                break;
+            case 1:
+                playerScore++;
+                output += `you win! The score is now: \n Computer: ${computerScore} \n You: ${playerScore}`;
+                result.textContent = output;
+                break;
+            case 2:
+                computerScore++;
+                output += `you lose. The score is now: \n Computer: ${computerScore} \n You: ${playerScore}`;
+                result.textContent = output; //TODO: Merge the three identical lines of code into one
+        }
+    }
+}
+
+
+function checkWin(playerScore, computerScore) {
+    if (playerScore == 5) {
+        result.textContent = "congratulations, you won!";
+    } else if (computerScore == 5) {
+        result.textContent = "game over, you lost :(";
+    }
+}
+
+let rockPress = false;
+let paperPress = false;
+let scissorsPress = false;
+
+let rockButton = document.querySelector("#rockButton");
+let paperButton = document.querySelector("#paperButton");
+let scissorsButton = document.querySelector("#scissorsButton");
+
+rockButton.addEventListener("click", () => { rockPress = true; playRound(playerScore, computerScore, rockPress, paperPress, scissorsPress) })
+paperButton.addEventListener("click", () => { paperPress = true; playRound(playerScore, computerScore, rockPress, paperPress, scissorsPress) })
+scissorsButton.addEventListener("click", () => { scissorsPress = true; playRound(playerScore, computerScore, rockPress, paperPress, scissorsPress) })
+
+
+
+//buttonPress.addEventListener("click", () => playRound(playerScore, computerScore));
+
+function WinDecider(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) {
+        return 0; //tie
+    }
+
+    //if the player returns ROCK
+    if (playerChoice == "ROCK") {
+        if (computerChoice == "SCISSORS") {
+            return 1; //win
+        } else {
+            return 2; //lose
         }
     }
 
-
-    function checkWin(playerScore, computerScore) {
-        if (playerScore == 5) {
-            result.textContent = "congratulations, you won!";
-        } else if (computerScore == 5) {
-            result.textContent = "game over, you lost :(";
+    if (playerChoice == "PAPER") {
+        if (computerChoice == "ROCK") {
+            return 1; //win
+        } else {
+            return 2; //lose
         }
     }
 
-    const buttonPress = document.querySelector("#anyButton");
-    buttonPress.addEventListener("click", () => playRound(playerScore, computerScore));
-
-    function WinDecider(playerChoice, computerChoice) {
-        if (playerChoice == computerChoice) {
-            return 0; //tie
-        }
-
-        //if the player returns ROCK
-        if (playerChoice == "ROCK") {
-            if (computerChoice == "SCISSORS") {
-                return 1; //win
-            } else {
-                return 2; //lose
-            }
-        }
-
-        if (playerChoice == "PAPER") {
-            if (computerChoice == "ROCK") {
-                return 1; //win
-            } else {
-                return 2; //lose
-            }
-        }
-
-        if (playerChoice == "SCISSORS") {
-            if (computerChoice == "PAPER") {
-                return 1; //win
-            } else {
-                return 2; //lose
-            }
+    if (playerChoice == "SCISSORS") {
+        if (computerChoice == "PAPER") {
+            return 1; //win
+        } else {
+            return 2; //lose
         }
     }
+}
 
-    function getRandomIntInclusive(min, max) {
-        min = Math.ceil(min);
-        max - Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max - Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
