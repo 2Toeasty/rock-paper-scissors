50cef4574a3d8f87d63d0f5f6bdff9bf2aebbe9e
Abdullah Zafar <136666960+2Toeasty@users.noreply.github.com>
2024-05-04 14:58:15 -0400

    remove unnecessary comments

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
 
-
-/*
-functions needed:
-Player Choice [done]
-Computer Choice [done]
-Win Decider [done]
-Win Counter
-*/
-
-/*
-Player choice: [DONE]
-Message: Enter Rock, Paper, or Scissors
-Store user input in string variable playerChoice
-convert all chars in playerChoice to uppercase
-While (playerChoice != ROCK || PAPER || SCISSORS) {
-    playerChoice = prompt("please enter a valid move")
-    playerChoice = playerChoice.toUpperCase;
-}
-Return playerChoice
-*/
-
-
-
-function getPlayerChoice() {
+function getPlayerChoice() { //TODO: Change prompts to button options
     let playerChoice = prompt("Enter Rock, Paper, or Scissors");
     playerChoice = playerChoice.toUpperCase();
     while (playerChoice != "ROCK" && playerChoice != "PAPER" && playerChoice != "SCISSORS") {
@@ -32,13 +9,6 @@ function getPlayerChoice() {
     return playerChoice;
 }
 
-/*
-Computer choice
-let variable randomNum get a random number between one and three
-if the number is 1, return "ROCK"
-if the number is 2, return "PAPER"
-if the number is 3, return "SCISSORS"
-*/
 function getComputerChoice() {
     let randomNum = getRandomIntInclusive(1, 3);
     switch (randomNum) {
@@ -52,32 +22,6 @@ function getComputerChoice() {
             return "SCISSORS";
     }
 }
-
-
-
-/*
-create integer playerScore == 0
-create integer computerScore == 0
-
-create a loop that runs through the game until either computerScore == 5, or playerScore == 5
-
-string output = "the computer chose " [computerChoice]
-String playerChoice = playerChoice()
-String computerChoice = computerChoice()
-int winner = WinDecider(playerChoice, computerChoice)
-
-if winner == 0, output += "it's a tie", move to the next game.
-if winner == 1, output += "you win", playerScore++
-if winner == 2, output += "you lose", computerScore++
-output "the score is: /nComputer: computerScore /nPlayer: playerScore"
-
-[once loop has exited]
-
-if playerScore == 5, output "Congratulations, you won the game!"
-    else
-        "Game over, you lost :("
-
-*/
 
 let playerScore = 0;
 let computerScore = 0;
@@ -124,28 +68,6 @@ function playRound(playerScore, computerScore) {
 
     const buttonPress = document.querySelector("#anyButton");
     buttonPress.addEventListener("click", () => playRound(playerScore, computerScore));
-
-    /*
-    WinDecider (playerChoice, computerChoice)
-    if playerChoice equals computerChoice, return 0
-    if playerChoice is ROCK,
-        then if computerChoice is SCISSORS
-            return 1
-        else
-            return 2
-    
-    if playerChoice is PAPER
-        then if computerChoice is ROCK,
-            return 1
-        else
-            return 2
-    
-    if playerChoice is SCISSORS
-        then if computerChoice is PAPER
-            return 1
-        else
-            return 2
-    */
 
     function WinDecider(playerChoice, computerChoice) {
         if (playerChoice == computerChoice) {
